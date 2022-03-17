2176107cf187c4819237add2769f9ad42eadda48
abuu <shayxislomovabdurahmon@gmail.com>
2022-03-17 16:44:48 +0500

    1

--- src/services/mock/user/logout.ts
+++
@@ -1,9 +0,0 @@
-import MockAdapter from 'axios-mock-adapter/types'
-
-export const mockLogout = (mock: MockAdapter, url: string) => {
-  mock.onPost(url).reply(async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1500))
-
-    return [200]
-  })
-}
